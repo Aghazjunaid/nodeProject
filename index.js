dd0ea7679124fc1291eb7e89e019c097108a164f
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-06-20 21:44:41 +0530

    setup project

--- index.js
+++ index.js
@@ -1,38 +1,67 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
 const mongoose = require('mongoose');
-const User = require("./models/user");
+const bodyParser = require('body-parser');
 
 const app = express()
-const port = 5000
+const port = 5000;
 
-// parse application/x-www-form-urlencoded
+const User = require("./models/user")
+
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://localhost/node-project', {
+var mongoUrl = 'mongodb://127.0.0.1/ecommerceDB'
+mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true
 });
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
-
-app.post('/signup', (req, res) => {
-    let opt = req.body
-      let user = new User(opt);
-      user.save(function(error, doc){
-        if(error) {
-          res.send(String(error))
+ 
+app.post("/register", (req,res) => {
+    let userData = req.body;
+    let user = User(userData)
+    user.save(function(error,doc){
+        if(error){
+            res.send(String(error))
+        } else{
+            res.send(doc)
         }
-        res.send(doc);
-      })
-    });
+    })
+})
 
-app.listen(port, () => {
+app.get("/user", (req,res) => {
+    User.find({}, function(error,doc){
+        if(error){
+            res.send(String(error))
+        }
+        res.send(doc)
+    })
+})
+
+app.get("/user/:id", (req,res) => {
+    User.find({_id: req.params.id}, function(error,doc){
+        if(error){
+            res.send(String(error))
+        }
+        res.send(doc)
+    })
+})
+
+app.get("/email/:id", (req,res) => {
+    User.findOne({email: req.params.id}, function(error,doc){
+        if(error){
+            res.send(String(error))
+        }
+        res.send(doc)
+    })
+})
+
+
+app.listen(port, ()=>{
     console.log(`app listening at http://localhost:${port}`)
 })
--- models/user.js
+++ models/user.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String },
-    phone: { type: Number },
-    password: { type: String },
-}, { timestamps : true })
+var UserSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    mobile: {type: Number},
+    password: {type: String, required:true}
+}, {timestamps: true});
 
-User = mongoose.model("user", UserSchema)
-
-module.exports = User
+module.exports = mongoose.model("user", UserSchema)
